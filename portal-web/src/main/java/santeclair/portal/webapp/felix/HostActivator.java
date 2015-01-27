f5bf68d715acc598da2d896dcd0109c766616998
telligcirdec <cgillet@santeclair.fr>
2015-01-27 18:42:32 +0100

    modification du logreader
    Ajout de logback au niveau des logs ! Boum bitch

--- portal-web/src/main/java/santeclair/portal/webapp/felix/HostActivator.java
+++ portal-web/src/main/java/santeclair/portal/webapp/felix/HostActivator.java
@@ -1,5 +1,6 @@
 package santeclair.portal.webapp.felix;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -10,9 +11,11 @@ import org.osgi.framework.FrameworkListener;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceListener;
+import org.osgi.framework.Version;
 import org.osgi.service.log.LogEntry;
 import org.osgi.service.log.LogListener;
 import org.osgi.service.log.LogReaderService;
+import org.osgi.service.log.LogService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -20,12 +23,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class HostActivator implements BundleActivator, ServiceListener {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(HostActivator.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(HostActivator.class);
 
 	private BundleContext bundleContext = null;
 
 	@Override
-	public void start(BundleContext bundleContext) throws InvalidSyntaxException {
+	public void start(BundleContext bundleContext)
+			throws InvalidSyntaxException {
 		LOGGER.debug("Starting HostActivator");
 		this.bundleContext = bundleContext;
 
@@ -33,7 +38,9 @@ public class HostActivator implements BundleActivator, ServiceListener {
 
 			@Override
 			public void frameworkEvent(FrameworkEvent frameworkEvent) {
-				System.out.println("frameworkEvent : " + frameworkEvent.getSource().toString() + " (" + frameworkEvent.getType() + ")");
+				System.out.println("frameworkEvent : "
+						+ frameworkEvent.getSource().toString() + " ("
+						+ frameworkEvent.getType() + ")");
 
 			}
 		};
@@ -42,12 +49,15 @@ public class HostActivator implements BundleActivator, ServiceListener {
 
 			@Override
 			public void bundleChanged(BundleEvent bundleEvent) {
-				System.out.println("bundleEvent : " + bundleEvent.getSource().toString() + " (" + bundleEvent.getType() + ")");
+				System.out.println("bundleEvent : "
+						+ bundleEvent.getSource().toString() + " ("
+						+ bundleEvent.getType() + ")");
 
 			}
 		};
 
-		String filter = "(objectclass=" + LogReaderService.class.getName() + ")";
+		String filter = "(objectclass=" + LogReaderService.class.getName()
+				+ ")";
 
 		bundleContext.addServiceListener(this, filter);
 		bundleContext.addBundleListener(bundleListener);
@@ -75,15 +85,68 @@ public class HostActivator implements BundleActivator, ServiceListener {
 
 	@Override
 	public void serviceChanged(ServiceEvent serviceEvent) {
-		LogReaderService logReaderService = (LogReaderService) bundleContext.getService(serviceEvent.getServiceReference());
+		
+		LogReaderService logReaderService = (LogReaderService) bundleContext
+				.getService(serviceEvent.getServiceReference());
 
 		logReaderService.addLogListener(new LogListener() {
 
 			@Override
 			public void logged(LogEntry entry) {
-				System.out.println(" !!! Listener Maison !!! : " + entry.getMessage());
+				// System.out.println(" !!! Listener Maison !!! : " +
+				// entry.getMessage());
+				switch (entry.getLevel()) {
+				case LogService.LOG_DEBUG:
+					if (LOGGER.isDebugEnabled()) {
+						LOGGER.debug(buildLogMessage(entry).toString());
+					}
+					break;
+				case LogService.LOG_INFO:
+					if (LOGGER.isInfoEnabled()) {
+						LOGGER.info(buildLogMessage(entry).toString());
+					}
+					break;
+				case LogService.LOG_WARNING:
+					if (LOGGER.isWarnEnabled()) {
+						LOGGER.warn(buildLogMessage(entry).toString());
+					}
+					break;
+				case LogService.LOG_ERROR:
+					if (LOGGER.isErrorEnabled()) {
+						LOGGER.error(buildLogMessage(entry).toString());
+					}
+					break;
+				default:
+					LOGGER.debug(buildLogMessage(entry).toString());
+					break;
+				}
 			}
 		});
+	}
+
+	private StringBuffer buildLogMessage(LogEntry entry) {
+		StringBuffer sbf = new StringBuffer();
+		if (entry != null) {
+			Bundle bundle = entry.getBundle();
+			if (bundle != null) {
+				sbf.append("[BUNDLE : ").append(bundle.getSymbolicName());
+				Version version = bundle.getVersion();
+				if (version != null) {
+					sbf.append(" | ").append(version.toString()).append("");
+				}
+				sbf.append(" (").append(bundle.getBundleId()).append(")")
+						.append("] - ");
+			}
+			sbf.append(entry.getMessage());
+			Throwable t = entry.getException();
+			if (t != null) {
+				sbf.append("\n\n").append("=========================\n")
+						.append("= EXCEPTION \n")
+						.append("=========================\n\n")
+						.append(ExceptionUtils.getStackTrace(t));
+			}
+		}
+		return sbf;
 	}
 
 }
