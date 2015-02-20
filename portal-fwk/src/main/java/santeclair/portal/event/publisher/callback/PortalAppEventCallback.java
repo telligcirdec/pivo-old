3065852b487f61a4766005d128285e6b0ca122fe
telligcirdec <cgillet@santeclair.fr>
2015-02-20 19:56:04 +0100

    Mise en place de la logique des publishers ! Nettoyage de printemps

--- portal-fwk/src/main/java/santeclair/portal/event/publisher/callback/PortalAppEventCallback.java
+++
@@ -1,9 +0,0 @@
-package santeclair.portal.event.publisher.callback;
-
-import santeclair.portal.vaadin.module.ModuleUiFactory;
-
-public interface PortalAppEventCallback {
-
-    public void addNewModuleUiFactory(ModuleUiFactory<?> moduleUiFactory);
-
-}
