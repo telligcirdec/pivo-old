a1c5d619a152123116338ac86d219c091a963196
telligcirdec <cgillet@santeclair.fr>
2015-03-26 15:59:18 +0100

    Mise en place de l'arborescence des dossiers de conf

--- portal-web/src/main/java/santeclair/portal/webapp/felix/config/FileInstallConfig.java
+++ portal-web/src/main/java/santeclair/portal/webapp/felix/config/FileInstallConfig.java
@@ -24,19 +24,21 @@ public class FileInstallConfig implements InitConfig {
         Map<String, Object> fileInstallConfig = new HashMap<>();
 
         fileInstallConfig
-                        .put("felix.fileinstall.dir", rootDir + "/auto-bundle");
+                        .put("felix.fileinstall.dir", rootDir + "/auto-bundle," + rootDir + "/config/module," + rootDir + "/config/view");
         fileInstallConfig.put("felix.fileinstall.tmpdir", rootDir
-                        + "/auto-bundle/tmp");
-        fileInstallConfig.put("felix.fileinstall.poll", "500");
+                        + "/config/tmp");
 
         if (LOGGER.isDebugEnabled() || LOGGER.isTraceEnabled()) {
             fileInstallConfig.put("felix.fileinstall.log.level", "4");
+            fileInstallConfig.put("felix.fileinstall.poll", "500");
         } else if (LOGGER.isInfoEnabled()) {
             fileInstallConfig.put("felix.fileinstall.log.level", "3");
         } else if (LOGGER.isWarnEnabled()) {
             fileInstallConfig.put("felix.fileinstall.log.level", "2");
+            fileInstallConfig.put("felix.fileinstall.poll", "2000");
         } else if (LOGGER.isErrorEnabled()) {
             fileInstallConfig.put("felix.fileinstall.log.level", "1");
+            fileInstallConfig.put("felix.fileinstall.poll", "2000");
         } else {
             fileInstallConfig.put("felix.fileinstall.log.level", "2");
         }
