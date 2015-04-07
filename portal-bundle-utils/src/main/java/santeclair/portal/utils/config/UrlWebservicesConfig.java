c4e22fa94584d0fd3748d49be2bef1379677dbf0
telligcirdec <telligcirdec91@gmail.com>
2015-04-07 21:02:43 +0200

    Changement des élements de package de bundle utils

---
+++ portal-bundle-utils/src/main/java/santeclair/portal/utils/config/UrlWebservicesConfig.java
@@ -0,0 +1,20 @@
+package santeclair.portal.utils.config;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Property;
+import org.apache.felix.ipojo.annotations.Provides;
+
+@Component(managedservice = "urlWebservices")
+@Provides(specifications = UrlWebservicesConfig.class)
+@Instantiate
+public class UrlWebservicesConfig {
+
+    @Property(name="url.webservice.reclamation")
+    private String reclamationUrl;
+
+    public String getReclamationUrl() {
+        return reclamationUrl;
+    }
+
+}
